d86d6e4187edfe9794315df4cea06036235ebeaa
Alex <alex.wollman@dsu.edu>
2025-10-09 19:49:20 -0500

    More debug.

--- core/mmu.cc
+++ core/mmu.cc
@@ -1337,6 +1337,7 @@ void seed_generator(void **s){
 }
 
 bool check_rdrand_support(){
+    debug_always("check_rdrand_support\n");
     unsigned int eax, ebx, ecx, edx;
     __asm__ __volatile__("cpuid"
         : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
